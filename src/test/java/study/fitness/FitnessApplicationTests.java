b2472a9f24a980d165c913ccee0a5613e290a069
pjh9898 <55084024+pjh9898@users.noreply.github.com>
2023-12-11 14:41:31 +0900

    파일 초기설정

---
+++ src/test/java/study/fitness/FitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package study.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
