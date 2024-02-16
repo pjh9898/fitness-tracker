3add9a4248ab89d0dc87573c9c1696c39f115652
pjh9898 <55084024+pjh9898@users.noreply.github.com>
2024-02-16 10:14:46 +0900

    swagger 추가

--- src/main/java/study/fitness/dto/LoginRequestDto.java
+++ src/main/java/study/fitness/dto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package study.fitness.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,17 @@ public class LoginRequestDto {
 
     private String userId;
     private String password;
+
+    @Builder
+    private LoginRequestDto(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    static public LoginRequestDto of(String userId, String password) {
+        return LoginRequestDto.builder()
+                .userId(userId)
+                .password(password)
+                .build();
+    }
 }
