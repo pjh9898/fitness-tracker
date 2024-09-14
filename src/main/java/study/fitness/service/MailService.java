15680c8d8280d7751a24c8b0ac4ccb3e334ba815
pjh9898 <55084024+pjh9898@users.noreply.github.com>
2024-09-14 23:00:39 +0900

    이메일 인증 추가

---
+++ src/main/java/study/fitness/service/MailService.java
@@ -0,0 +1,65 @@
+package study.fitness.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import java.util.Random;
+
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+    private static final String senderEmail = "workoutroutinary@gmail.com";
+
+    // 랜덤으로 숫자 생성
+    public String createNumber() {
+        Random random = new Random();
+        StringBuilder key = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+            int index = random.nextInt(3);
+
+            switch (index) {
+                case 0 -> key.append((char) (random.nextInt(26) + 97));
+                case 1 -> key.append((char) (random.nextInt(26) + 65));
+                case 2 -> key.append(random.nextInt(10));
+            }
+        }
+        return key.toString();
+    }
+
+    public MimeMessage createMail(String mail, String number) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.setFrom(senderEmail);
+        message.setRecipients(MimeMessage.RecipientType.TO, mail);
+        message.setSubject("이메일 인증");
+        String body = "";
+        body += "<h3>이메일 인증번호는</h3>";
+        body += "<h1>" + number + "입니다</h1>";
+        body += "<h3>감사합니다.</h3>";
+        message.setText(body, "UTF-8", "html");
+
+        return message;
+    }
+
+    // 메일 발송
+    public String sendSimpleMessage(String sendEmail) throws MessagingException {
+        String number = createNumber();
+
+        MimeMessage message = createMail(sendEmail, number);
+        try {
+            javaMailSender.send(message); //
+        } catch (MailException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("메일 발송 중 오류가 발생했습니다.");
+        }
+
+        return number;
+    }
+}
